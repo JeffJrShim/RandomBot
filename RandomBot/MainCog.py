30287d064350d0c95814ae4674617de86dc6dd2f
Tom E <guacaplushy@icloud.com>
2021-08-07 09:42:47 -0400

    Update MainCog.py

--- RandomBot/MainCog.py
+++ RandomBot/MainCog.py
@@ -2,6 +2,9 @@ import RandomBot.Values
 import RandomBot.Choosers
 import RandomBot.Shards
 import RandomBot.Shufflers
+import RandomBot.Hidden
+import RandomBot.Generators
+import RandomBot.Events
 
 from discord.ext import commands
 
@@ -10,3 +13,6 @@ def setup(bot : commands.Bot):
   bot.add_cog(Choosers(main=bot))
   bot.add_cog(Shards(main=bot))
   bot.add_cog(Shufflers(main=bot))
+  bot.add_cog(Hidden(main=bot))
+  bot.add_cog(Generators(main=bot))
+  bot.add_cog(Events(main=bot))
