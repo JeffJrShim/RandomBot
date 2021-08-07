24cc55da99549dd3991579ff4e62a5dc0d96eff0
Tom E <guacaplushy@icloud.com>
2021-08-07 09:57:08 -0400

    Update MainCog.py

--- RandomBot/MainCog.py
+++ RandomBot/MainCog.py
@@ -8,11 +8,15 @@ from RandomBot.Events import *
 
 from discord.ext import commands
 
+class RandomBotCog(commands.Cog, Values, Choosers, Shards, Shufflers, Hidden, Generators, Events):
+  "I'm not empty!"
+
 def setup(bot : commands.Bot):
-  bot.add_cog(Values(main=bot))
+  '''bot.add_cog(Values(main=bot))
   bot.add_cog(Choosers(main=bot))
   bot.add_cog(Shards(main=bot))
   bot.add_cog(Shufflers(main=bot))
   bot.add_cog(Hidden(main=bot))
   bot.add_cog(Generators(main=bot))
-  bot.add_cog(Events(main=bot))
+  bot.add_cog(Events(main=bot))'''
+  bot.add_cog(RandomBotCog(main=bot))
