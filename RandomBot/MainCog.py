463447268d59d0c5410d7f363bfb6ee8a2c46bcd
Tom E <guacaplushy@icloud.com>
2021-08-07 10:09:22 -0400

    Update MainCog.py

--- RandomBot/MainCog.py
+++ RandomBot/MainCog.py
@@ -71,6 +71,35 @@ class RandomBotCog(commands.Cog, *Cogs):
       ]
       await ctx.send(', '.join(vdice))
   #End Values.py
+  
+  #Start Shufflers.py
+  @commands.command(name='shuffle', help='Shuffle word(s)')
+  async def shuffler(self, ctx, *, word: str):
+      wordl = list(word)
+      random.shuffle(wordl)
+      await ctx.send(''.join(wordl))
+  
+  @commands.command(name='shufflebyword', help='Shuffle every word in a sentence.')
+  async def shuffleword(self, ctx, *, wordl: str):
+      wordl = wordl.split(" ")
+      newsentence = []
+      for byword in range(len(wordl)):
+        wordl1 = list(wordl[byword])
+        wordl2 = random.sample(wordl1, len(wordl1))
+        newsentence1 = ""
+        for wjoin in wordl2:
+          newsentence1 += wjoin
+        newsentence.append(newsentence1 + ' ')
+      wordl = newsentence
+      await ctx.send(''.join(wordl))
+
+  @commands.command(name='shufflesentence', help='Shuffle words to make a weird sentence.')
+  async def makesentence(self, ctx, *, sentence: str):
+      wordlist = sentence.split(' ')
+      wordlist1 = list(wordlist)
+      random.shuffle(wordlist1)
+      await ctx.send(' '.join(wordlist1))
+  #End Shufflers.py
 
 def setup(bot : commands.Bot):
   '''bot.add_cog(Values(main=bot))
