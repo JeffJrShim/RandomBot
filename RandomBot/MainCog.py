2ce164cafa84754c7ef0f37a59999041a9faa90d
Tom E <guacaplushy@icloud.com>
2021-08-08 12:31:27 -0400

    Update MainCog.py

--- RandomBot/MainCog.py
+++ RandomBot/MainCog.py
@@ -92,32 +92,6 @@ class RandomBot(commands.Cog):
       await ctx.send(' '.join(wordlist1))
   #End Shufflers.py
   
-  #Start Shards.py
-  @commands.Cog.listener()
-  async def on_shard_connect(self,shard_id):
-    if shard_id == 0:
-      activity = discord.Game(name=f"Starting shard {shard_id+1}.")
-      await self.bot.change_presence(status=discord.Status.online, activity=activity)
-      print(f"{self.bot.user} has connected.")
-    else:
-      activity = discord.Game(name=f"Starting shard {shard_id+1}.")
-      await self.bot.change_presence(status=discord.Status.online, activity=activity)
-      print(f"Shard {shard_id} has connected.")
-      print(f"Shard {shard_id+1} is connecting.")
-
-  @commands.Cog.listener()
-  async def on_shard_disconnect(self,shard_id):
-    print(f"Shard {shard_id} has disconnected.")
-
-  @commands.Cog.listener()
-  async def on_shard_resume(self,shard_id):
-    print(f"Shard {shard_id} has reconnected.")
-
-  @commands.Cog.listener()
-  async def on_shard_ready(self,shard_id):
-    print(f"Shard {shard_id} is ready to go!")
-  #End Shards.py
-  
   #Start Hidden.py
   @commands.command(name="rbsupport", hidden=True)
   async def support(self, ctx):
